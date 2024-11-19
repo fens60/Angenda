7ec4ed4c4cdc5ca7adc938fa10bdcb1995266671
fens60 <shangfeng2003@gmail.com>
2024-11-19 14:04:01 +0100

    Trabajo realizado

--- src/main/java/es/ieslosmontecillos/AppAgendaBE/AppAgendaBeApplication.java
+++ src/main/java/es/ieslosmontecillos/AppAgendaBE/AppAgendaBeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppAgendaBeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AppAgendaBeApplication.class, args);
     }
 
